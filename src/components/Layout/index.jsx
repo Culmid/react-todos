d2e2d6e7e485a8a6a3bdbb9a659eda6c3737c06e
Cullen Middleton <culmid@gmail.com>
2022-05-06 13:25:39 +0200

    Layout main

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,10 +3,11 @@ import Footer from "../Footer";
 import Header from "../Header";
 import styles from "./Layout.module.css";
 
-function Layout() {
+function Layout({ children }) {
   return (
     <div className={styles["page-wrapper"]}>
       <Header />
+      <main className={styles.content}>{children}</main>
       <Footer />
     </div>
   );
