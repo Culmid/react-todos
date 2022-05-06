aa50453f5807cb7f4becf433569286e6012e09c5
Cullen Middleton <culmid@gmail.com>
2022-05-06 13:13:26 +0200

    Nav

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Header.module.css";
+import Nav from "../Nav";
 
 function Header() {
   return (
     <header>
       <h1 className={styles.logo}>Todo List</h1>
+      <Nav />
     </header>
   );
 }
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Nav.module.css";
+
+function Nav() {
+  return (
+    <nav>
+      <ul className={styles["nav-list"]}>
+        <li className={styles["nav-entry"]}>
+          <Link className={styles["nav-link"]} to="/">
+            Home
+          </Link>
+        </li>
+        <li className={styles["nav-entry"]}>
+          <Link className={styles["nav-link"]} to="/todos">
+            Todos
+          </Link>
+        </li>
+        <li className={styles["nav-entry"]}>
+          <Link className={styles["nav-link"]} to="/about">
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
