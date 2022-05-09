c15b34b4736711c1af86900810e4fa8091521f7d
Cullen Middleton <culmid@gmail.com>
2022-05-09 15:34:39 +0200

    About page + README

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -6,22 +6,21 @@ function About() {
     <div className={styles["about-container"]}>
       <h2 className={styles["about-header"]}>About</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum
-        felis a nunc tempus mattis. Aliquam vel eros rutrum, rhoncus odio sit
-        amet, porttitor arcu. Maecenas lectus est, hendrerit nec convallis id,
-        ornare a tellus.
+        This App is the final project for the React section of the Graduate
+        Program at Yoco. We are tasked to build a Todo list where we demonstrate
+        knowledge of props, state, basic styling in React, props drilling,
+        state-lifting and `useReducer`.
       </p>
       <p>
-        Vestibulum eu dignissim orci, eu congue diam. Suspendisse potenti. Ut
-        cursus, mauris vel semper fermentum, mi lectus euismod ligula, in
-        feugiat justo ligula ullamcorper ex. Nam sit amet dui ultricies, iaculis
-        ex eget, bibendum urna. Aliquam bibendum mauris posuere metus elementum
-        tempus ut sit amet ligula.
+        This App is constructed using a Figma design as a basis. See the project
+        repo for reference images. However, it is worth noting that the idea of
+        the project was not styling. Despite this, some visual improvements have
+        been made over the initial design.
       </p>
       <p>
-        Praesent ultrices condimentum risus, in viverra odio hendrerit ac. Nunc
-        pretium non nulla sed blandit. Suspendisse a semper tortor. Nunc mattis
-        velit quis maximus semper.
+        Please see the GitHub repo for more information on the project. Any
+        recommendations are welcome, as well as offers of help. I hope this
+        project is helpful.
       </p>
     </div>
   );
