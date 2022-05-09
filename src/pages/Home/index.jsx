bf1c2a6cc7b64b050391d4b0f333fb08fe46e197
Cullen Middleton <culmid@gmail.com>
2022-05-09 13:36:34 +0200

    Random quotes on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import styles from "./Home.module.css";
+import { useEffect, useState } from "react";
+import { GrRefresh } from "react-icons/gr";
 
 function Home() {
-  return <div>Home</div>;
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random?maxLength=200")
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
+      });
+  }, []);
+
+  return (
+    <div className={styles["home-div"]}>
+      {quote ? (
+        <figure>
+          <blockquote>
+            <p>{quote.content}</p>
+          </blockquote>
+          <figcaption>—{quote.author}</figcaption>
+        </figure>
+      ) : (
+        <GrRefresh className={styles.loading} />
+      )}
+    </div>
+  );
 }
 
 export default Home;
