7964c8cda2a2cb9cefeaec6b0e0a03dfec5e8767
Cullen Middleton <culmid@gmail.com>
2022-05-10 15:30:16 +0200

    Strict TodoList

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,15 +1,48 @@
-import { useReducer, useState } from "react";
+import React, { useReducer, useState } from "react";
 import Todo from "../Todo";
 import styles from "./TodoList.module.css";
 import { initialState, reducer, TodoType } from "../../reducers/todos";
 import NewTodoForm from "../NewTodoForm";
 import { FiPlus, FiRefreshCw } from "react-icons/fi";
 
-function TodoList() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [isFormVisible, setFormVisible] = useState(false);
+function TodoList(): JSX.Element {
+  const [state, dispatch]: [
+    TodoType[],
+    React.Dispatch<{
+      type: string;
+      payload?:
+        | { title: string; background: string; color: string }
+        | { id: number }
+        | undefined;
+    }>
+  ] = useReducer<
+    (
+      state: TodoType[],
+      action: {
+        type: string;
+        payload?:
+          | {
+              title: string;
+              background: string;
+              color: string;
+            }
+          | {
+              id: number;
+            }
+          | undefined;
+      }
+    ) => TodoType[]
+  >(reducer, initialState);
+  const [isFormVisible, setFormVisible]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>
+  ] = useState<boolean>(false);
 
-  function onAddNewTodo(title: string, background: string, color: string) {
+  function onAddNewTodo(
+    title: string,
+    background: string,
+    color: string
+  ): void {
     dispatch({
       type: "addTodo",
       payload: { title, background, color },
@@ -17,23 +50,23 @@ function TodoList() {
     setFormVisible(false);
   }
 
-  function onDeleteTodo(id: number) {
+  function onDeleteTodo(id: number): void {
     dispatch({ type: "deleteTodo", payload: { id } });
   }
 
-  function onResetButtonClick() {
+  function onResetButtonClick(): void {
     dispatch({ type: "resetTodos" });
   }
 
-  function onAddTodoClick() {
+  function onAddTodoClick(): void {
     setFormVisible(true);
   }
 
-  function onToggleTodo(id: number) {
+  function onToggleTodo(id: number): void {
     dispatch({ type: "toggleTodo", payload: { id } });
   }
 
-  function onFormExit() {
+  function onFormExit(): void {
     setFormVisible(false);
   }
 
@@ -49,7 +82,7 @@ function TodoList() {
       </div>
       {state.find((todo: TodoType) => !todo.isComplete) ? (
         <div>
-          <h2>To Do</h2>
+          <h2 className={styles["list-header"]}>To Do</h2>
           <div>
             {state
               .filter((todo: TodoType) => !todo.isComplete)
@@ -70,7 +103,7 @@ function TodoList() {
       ) : null}
       {state.find((todo: TodoType) => todo.isComplete) ? (
         <div>
-          <h2>Completed</h2>
+          <h2 className={styles["list-header"]}>Completed</h2>
           <div>
             {state
               .filter((todo: TodoType) => todo.isComplete)
