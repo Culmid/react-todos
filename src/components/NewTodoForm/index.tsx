b7697379e7b9769ec47ab18c9d02b8c6da231b23
Cullen Middleton <culmid@gmail.com>
2022-05-10 15:18:46 +0200

    Strict NewTodoForm

--- src/components/NewTodoForm/index.tsx
+++ src/components/NewTodoForm/index.tsx
@@ -8,24 +8,33 @@ function NewTodoForm({
 }: {
   onAddNewTodo: (title: string, background: string, color: string) => void;
   onFormExit: () => void;
-}) {
-  const [title, setTitle] = useState("");
-  const [background, setBackground] = useState("#c4c4c4");
-  const [color, setColor] = useState("#000000");
+}): JSX.Element {
+  const [title, setTitle]: [
+    string,
+    React.Dispatch<React.SetStateAction<string>>
+  ] = useState<string>("");
+  const [background, setBackground]: [
+    string,
+    React.Dispatch<React.SetStateAction<string>>
+  ] = useState<string>("#c4c4c4");
+  const [color, setColor]: [
+    string,
+    React.Dispatch<React.SetStateAction<string>>
+  ] = useState<string>("#000000");
 
-  function onTitleChange(event: React.FormEvent<HTMLInputElement>) {
+  function onTitleChange(event: React.FormEvent<HTMLInputElement>): void {
     setTitle(event.currentTarget.value);
   }
 
-  function onBackgroundChange(event: React.FormEvent<HTMLInputElement>) {
+  function onBackgroundChange(event: React.FormEvent<HTMLInputElement>): void {
     setBackground(event.currentTarget.value);
   }
 
-  function onColorChange(event: React.FormEvent<HTMLInputElement>) {
+  function onColorChange(event: React.FormEvent<HTMLInputElement>): void {
     setColor(event.currentTarget.value);
   }
 
-  function onFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+  function onFormSubmit(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault();
     onAddNewTodo(title, background, color);
   }
@@ -35,6 +44,7 @@ function NewTodoForm({
       <h1>Add Todo</h1>
       <label htmlFor="title">Title</label>
       <input
+        className={styles["form-input"]}
         type="text"
         name="title"
         value={title}
@@ -43,6 +53,7 @@ function NewTodoForm({
       ></input>
       <label htmlFor="background">Background</label>
       <input
+        className={styles["form-input"]}
         type="color"
         name="background"
         value={background}
@@ -51,6 +62,7 @@ function NewTodoForm({
       />
       <label htmlFor="color">Colour</label>
       <input
+        className={styles["form-input"]}
         type="color"
         name="color"
         value={color}
